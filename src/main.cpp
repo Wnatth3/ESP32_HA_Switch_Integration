ca06f0be764f8d79c5965440d8f20623d1c57a24
Phattharakorn Mahasorasak <foolproof.ja@gmail.com>
2025-06-22 23:37:40 +0700

    Init

--- src/main.cpp
+++ src/main.cpp
@@ -23,24 +23,40 @@
 #define btnPin    0
 #define haBtnName "LedBtn"
 
+#define ledPin1    18
+#define btnPin1    22
+#define haBtnName1 "LedBtn1"
+
 Button2 ledBtn;
+Button2 ledBtn1;
 
 WiFiClient client;
 HADevice   device;
 HAMqtt     mqtt(client, device);
 
 HASwitch ledSw("LedSw");  // "myledSw" is a unique ID as a part of MQTT topic that will be used to control the switch.
+HASwitch ledSw1("LedSw1");  // "myledSw" is a unique ID as a part of MQTT topic that will be used to control the switch.
 
 HADeviceTrigger toggleLedBtn(HADeviceTrigger::ButtonShortPressType, haBtnName);  // You have to add automation in Home Assistant to handle this trigger
+HADeviceTrigger toggleLedBtn1(HADeviceTrigger::ButtonShortPressType, haBtnName1);  // You have to add automation in Home Assistant to handle this trigger
 
 void toggleLed(Button2& btn) {
     Serial.println("tap");
     toggleLedBtn.trigger();
     digitalWrite(ledPin, !digitalRead(ledPin));  // Uncomment this line to toggle the LED while offline
 }
+void toggleLed1(Button2& btn) {
+    Serial.println("tap1");
+    toggleLedBtn1.trigger();
+    digitalWrite(ledPin1, !digitalRead(ledPin1));  // Uncomment this line to toggle the LED while offline
+}
 
 void onSwitchCommand(bool state, HASwitch* sender) {
     digitalWrite(ledPin, (state ? HIGH : LOW));
+    sender->setState(state);  // report state back to the Home Assistant
+}
+void onSwitchCommand1(bool state, HASwitch* sender) {
+    digitalWrite(ledPin1, (state ? HIGH : LOW));
     sender->setState(state);  // report state back to the Home Assistant
 }
 
@@ -48,9 +64,13 @@ void setup() {
     Serial.begin(115200);
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
+    pinMode(ledPin1, OUTPUT);
+    digitalWrite(ledPin1, LOW);
 
     ledBtn.begin(btnPin);
     ledBtn.setTapHandler(toggleLed);
+    ledBtn1.begin(btnPin1);
+    ledBtn1.setTapHandler(toggleLed1);
 
     byte mac[6];
     WiFi.macAddress(mac);
@@ -64,8 +84,11 @@ void setup() {
 
     device.setName("My ESP32");          // Device name
     device.setSoftwareVersion("1.0.0");  // Device software version
+
     ledSw.setName("LED Switch");         // Switch name
     ledSw.onCommand(onSwitchCommand);
+    ledSw1.setName("LED Switch1");         // Switch name
+    ledSw1.onCommand(onSwitchCommand1);
 
     mqtt.begin(mqttBrokerIp, mqttUser, mqttPassword);
 }
@@ -73,4 +96,5 @@ void setup() {
 void loop() {
     mqtt.loop();
     ledBtn.loop();
+    ledBtn1.loop();
 }
