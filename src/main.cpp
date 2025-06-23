55cae976ee7950fb73f745312b6aa914d0682925
Phattharakorn Mahasorasak <foolproof.ja@gmail.com>
2025-06-23 09:57:55 +0700

    Init multiBt

--- src/main.cpp
+++ src/main.cpp
@@ -19,44 +19,59 @@
 */
 #include "Secret.h"
 
-#define ledPin    LED_BUILTIN
-#define btnPin    0
-#define haBtnName "LedBtn"
+#define ledPin    23
+#define btnPin    17
+#define haBtnName "LedBtn" // "LedBtn" is a unique Trigger name that will be used in Home Assistant to trigger automations.
 
-#define ledPin1    18
-#define btnPin1    22
+#define ledPin1    19
+#define btnPin1    16
 #define haBtnName1 "LedBtn1"
 
-Button2 ledBtn;
-Button2 ledBtn1;
+#define ledPin2    18
+#define btnPin2    4
+#define haBtnName2 "LedBtn2"
 
 WiFiClient client;
 HADevice   device;
 HAMqtt     mqtt(client, device);
 
-HASwitch ledSw("LedSw");  // "myledSw" is a unique ID as a part of MQTT topic that will be used to control the switch.
-HASwitch ledSw1("LedSw1");  // "myledSw" is a unique ID as a part of MQTT topic that will be used to control the switch.
+Button2 ledBtn;
+Button2 ledBtn1;
+Button2 ledBtn2;
 
-HADeviceTrigger toggleLedBtn(HADeviceTrigger::ButtonShortPressType, haBtnName);  // You have to add automation in Home Assistant to handle this trigger
-HADeviceTrigger toggleLedBtn1(HADeviceTrigger::ButtonShortPressType, haBtnName1);  // You have to add automation in Home Assistant to handle this trigger
+HASwitch ledSw("LedSw");    // "ledSw" is a unique ID as a part of MQTT topic that will be used to control the switch.
+HASwitch ledSw1("LedSw1");  
+HASwitch ledSw2("LedSw2");  
+
+HADeviceTrigger toggleLedBtn(HADeviceTrigger::ButtonShortPressType, haBtnName);    // You have to add automation in Home Assistant to handle this trigger
+HADeviceTrigger toggleLedBtn1(HADeviceTrigger::ButtonShortPressType, haBtnName1);  
+HADeviceTrigger toggleLedBtn2(HADeviceTrigger::ButtonShortPressType, haBtnName2);  
 
 void toggleLed(Button2& btn) {
-    Serial.println("tap");
-    toggleLedBtn.trigger();
-    digitalWrite(ledPin, !digitalRead(ledPin));  // Uncomment this line to toggle the LED while offline
-}
-void toggleLed1(Button2& btn) {
-    Serial.println("tap1");
-    toggleLedBtn1.trigger();
-    digitalWrite(ledPin1, !digitalRead(ledPin1));  // Uncomment this line to toggle the LED while offline
+    if (btn == ledBtn) {
+        Serial.println("btn clicked");
+        toggleLedBtn.trigger();
+        digitalWrite(ledPin, !digitalRead(ledPin));
+    } else if (btn == ledBtn1) {
+        Serial.println("btn1 clicked");
+        toggleLedBtn1.trigger();
+        digitalWrite(ledPin1, !digitalRead(ledPin1));
+    } else if (btn == ledBtn2) {
+        Serial.println("btn2 clicked");
+        toggleLedBtn2.trigger();
+        digitalWrite(ledPin2, !digitalRead(ledPin2));
+    }
 }
 
 void onSwitchCommand(bool state, HASwitch* sender) {
-    digitalWrite(ledPin, (state ? HIGH : LOW));
-    sender->setState(state);  // report state back to the Home Assistant
-}
-void onSwitchCommand1(bool state, HASwitch* sender) {
-    digitalWrite(ledPin1, (state ? HIGH : LOW));
+    if (sender == &ledSw) {
+        digitalWrite(ledPin, (state ? HIGH : LOW));
+    } else if (sender == &ledSw1) {
+        digitalWrite(ledPin1, (state ? HIGH : LOW));
+    } else if (sender == &ledSw2) {
+        digitalWrite(ledPin2, (state ? HIGH : LOW));
+    }
+
     sender->setState(state);  // report state back to the Home Assistant
 }
 
@@ -66,11 +81,15 @@ void setup() {
     digitalWrite(ledPin, LOW);
     pinMode(ledPin1, OUTPUT);
     digitalWrite(ledPin1, LOW);
+    pinMode(ledPin2, OUTPUT);
+    digitalWrite(ledPin2, LOW);
 
     ledBtn.begin(btnPin);
     ledBtn.setTapHandler(toggleLed);
     ledBtn1.begin(btnPin1);
-    ledBtn1.setTapHandler(toggleLed1);
+    ledBtn1.setTapHandler(toggleLed);
+    ledBtn2.begin(btnPin2);
+    ledBtn2.setTapHandler(toggleLed);
 
     byte mac[6];
     WiFi.macAddress(mac);
@@ -85,10 +104,12 @@ void setup() {
     device.setName("My ESP32");          // Device name
     device.setSoftwareVersion("1.0.0");  // Device software version
 
-    ledSw.setName("LED Switch");         // Switch name
+    ledSw.setName("LED Switch");  // Switch name
     ledSw.onCommand(onSwitchCommand);
-    ledSw1.setName("LED Switch1");         // Switch name
-    ledSw1.onCommand(onSwitchCommand1);
+    ledSw1.setName("LED Switch1");  // Switch name
+    ledSw1.onCommand(onSwitchCommand);
+    ledSw2.setName("LED Switch2");  // Switch name
+    ledSw2.onCommand(onSwitchCommand);
 
     mqtt.begin(mqttBrokerIp, mqttUser, mqttPassword);
 }
@@ -97,4 +118,5 @@ void loop() {
     mqtt.loop();
     ledBtn.loop();
     ledBtn1.loop();
+    ledBtn2.loop();
 }
